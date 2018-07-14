aa6bc5c89738dd683e9c07181a1f6f1db2008de5
emme8888 <emme8888@hotmail.com>
2018-07-14 08:37:52 -0500

    push

---
+++ assets/javascript.js/app.js
@@ -0,0 +1,9 @@
+var animals=["cow","monkey","dog","cat","turtle","tiger","lizard","bird","mouse","moose"];
+var animal=$("#gif-input").val();
+var queryURL
+
+function displayAnimal(){
+    animal=$(this).attr("data-name");
+    quaryURL=
+
+}
